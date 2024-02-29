bdfa95345abb705c4d5dbd80b71ba67f2e42b8a6
Nodirbotirov <nbotirov2000@gmail.com>
2024-02-29 10:49:03 +0500

    repository

--- src/main/java/com/nod/lone/model/Student.java
+++ src/main/java/com/nod/lone/model/Student.java
@@ -1,8 +1,10 @@
 package com.nod.lone.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 //@Getter
 //@Setter
@@ -10,13 +12,22 @@ import java.time.LocalDate;
 //@AllArgsConstructor
 //@RequiredArgsConstructor
 @Data
-@Builder
+@Entity
+@Table(name ="students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
-    @NonNull
+    @Column(unique = true)
     private String email;
+    @Transient
     private int age;
 
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears() + 1;
+    }
 }
---
+++ src/main/java/com/nod/lone/service/impl/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package com.nod.lone.service.impl;
+
+import com.nod.lone.model.Student;
+import com.nod.lone.repository.StudentRepository;
+import com.nod.lone.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Primary
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
+
+
+    @Override
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return repository.findStudentByEmail(email);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return repository.save(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudent(String email) {
+        repository.deleteByEmail(email);
+    }
+}
